a0725bc600dd73f2dcad6bf945a8ce014e65e150
Din Stolak <dee@dins-mbp.home>
2018-10-14 22:15:35 +0100

    Setup eslint also completed next test and pass.

--- assets/spec/bowledOver_spec.js
+++ assets/spec/bowledOver_spec.js
@@ -6,17 +6,25 @@ describe('Bowling', function() {
 
   beforeEach(function() {
     bowling = new Bowling();
-});
-
-describe("New score sheet", function() {
-  it('Creates an empty score sheet', function() {
-    expect(bowling).toEqual(bowling);
   });
-});
 
- describe('initially', function() {
-   it('frame: 1', function() {
-     expect(bowling.getFrame()).toEqual(1);
-   });
- });
+  describe("New score sheet", function() {
+    it('Creates an empty score sheet', function() {
+      expect(bowling).toEqual(bowling);
+    });
+  });
+
+  describe('Begins with', function() {
+    it('frame: 1', function() {
+      expect(bowling.getFrame()).toEqual(1);
+    });
+  });
+
+  describe('nextFrame', function() {
+    it('increments frame', function() {
+      bowling.nextFrame();
+      expect(bowling.getFrame()).toEqual(2);
+    });
+  });
+
 });
--- assets/src/bowledOver.js
+++ assets/src/bowledOver.js
@@ -1,7 +1,22 @@
- function Bowling() {
-   this.frame = 1;
+function Bowling() {
+  this.rollHistory = [];
+  this.frame = 1;
+  this.rollsThisFrame = 0;
+  this.pinsRemaining = 10;
 };
 
- Bowling.prototype.getFrame = function () {
-   return this.frame;
+Bowling.prototype.getFrame = function () {
+  return this.frame;
+};
+
+Bowling.prototype.nextFrame = function () {
+  this.rollsThisFrame = 0;
+  this.pinsRemaining = 10;
+  this.strikeThisFrame = false;
+  if (this.frame < 11) {
+    this.frame += 1;
+  } 
+  else {
+    this.frame = 11;
+  }
 };
