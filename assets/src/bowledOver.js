3e94fd0773cfb5054b4d270f4c76ff1012099979
Din Stolak <dee@dins-mbp.home>
2018-10-14 23:29:06 +0100

    Low energy, believe my tests are failing as I'm not using the same instance instead I'm re-rolling.

--- assets/src/bowledOver.js
+++ assets/src/bowledOver.js
@@ -1,5 +1,5 @@
 function Bowling() {
-  this.rollHistory = [];
+  this.rollAccumulator = [];
   this.frame = 1;
   this.rollsThisFrame = 0;
   this.pinsSet = 10;
@@ -31,11 +31,63 @@ Bowling.prototype.isFrameComplete = function () {
 
 Bowling.prototype.roll = function (pins) {
   if (pins > this.pinsSet) {
-    throw new TypeError('Please enter a valid number of pins');
+    throw new TypeError('Please enter a valid number of pins!');
   } else {
     this.rollsThisFrame += 1;
-    this.rollHistory.push(pins);
+    this.rollAccumulator.push(pins);
     this.pinsSet -= pins;
     this.isFrameComplete();
   }
+};
+
+Bowling.prototype.getScore = function () {
+  let gameScore = 0;
+  let rollIndex = 0;
+  let game = this;
+
+  for (let frameIndex = 0; frameIndex < this.frameNumber-1; frameIndex++) {
+    if (isStrike()) {
+      gameScore += getStrikeScore();
+      rollIndex += 1;
+    } 
+    else if (isSpare()) {
+      gameScore += getSpareScore();
+      rollIndex += 2;
+    } 
+    else {
+      gameScore += getNormalScore();
+      rollIndex += 2;
+    }
+  };
+  return gameScore;
+
+  function isStrike() {
+    return game.rollAccumulator[rollIndex] === 10;
+  };
+
+  function isSpare() {
+    return game.rollAccumulator[rollIndex] + game.rollAccumulator[rollIndex + 1] === 10;
+  };
+
+  function getStrikeScore() {
+    if (typeof game.rollAccumulator[rollIndex + 2] === 'undefined') {
+        return 0;
+    } 
+    else {
+      return game.rollAccumulator[rollIndex] + game.rollAccumulator[rollIndex + 1] + game.rollAccumulator[rollIndex + 2];
+    }
+  };
+
+  function getSpareScore() {
+    if (typeof game.rollAccumulator[rollIndex + 2] === 'undefined') {
+        return 0;
+    } 
+    else {
+        return game.rollAccumulator[rollIndex] + game.rollAccumulator[rollIndex + 1] + game.rollAccumulator[rollIndex + 2];
+    }
+  };
+
+  function getNormalScore() {
+    return game.rollAccumulator[rollIndex] + game.rollAccumulator[rollIndex + 1];
+  };
 };
